779b9714a93412b86461b9632bf2003ab4e1d351
laspruebas <laspruebas.ar@gmail.com>
2026-09-18 10:11:06 -0300

    Use shared HTML escaping utility

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 import { API_BASE } from "./config.js";
 import { validateSessionToken } from "./session.js";
+import { escapeHtml } from "./utils.js";
 
 // === COLORS ===
 // Orden alineado con UX: frutas → verduras base → complemento
@@ -72,15 +73,6 @@ function setStatus(message, type = "") {
   if (type) {
     statusEl.classList.add(type);
   }
-}
-
-function escapeHtml(text) {
-  return String(text)
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
 }
 
 function handleImageError(img) {
